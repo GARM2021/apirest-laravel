6baa81344818c5adb3294b120a79ab5d4f4379be
GARM2021 <ivanhoe.real@gmail.com>
2022-03-28 18:13:16 -0600

    C27 Model Clientes guardando datos

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -6,55 +6,76 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash; //! C26
+use App\Models\Clientes; //! C27
 
 class ClientesController extends Controller
 {
-    
 
-   public function index()
-   {
-       $json = array (
-           "detalle"=>"no encontrado"
-       );
 
-       echo json_encode($json, true);//! C22 convierte array a json_decode //convierte un jason en array 
+    public function index()
+    {
+        $json = array(
+            "detalle" => "no encontrado"
+        );
 
- 
-   
+        echo json_encode($json, true); //! C22 convierte array a json_decode //convierte un jason en array 
+
+
+
     }
 
-     /*======================================================================================*/
-                          //! Crear un registro                                                             
-     /*======================================================================================*/
+    /*======================================================================================*/
+    //! Crear un registro                                                             
+    /*======================================================================================*/
 
-      public function store(Request $request)
-      {
-    //      $datos =   array("primer_nombre"=>$request->input("primer_nombre"));
-    //     // echo '<pre>'; print_r($datos); echo '</pre>';
-        $datos = array("primer_nombre"=>$request->input("primer_nombre"), "primer_apellido"=>$request->input("primer_apellido"), "email"=>$request->input("email"));  //! C24
+    public function store(Request $request)
+    {
+        //      $datos =   array("primer_nombre"=>$request->input("primer_nombre"));
+        //     // echo '<pre>'; print_r($datos); echo '</pre>';
+        $datos = array("primer_nombre" => $request->input("primer_nombre"), "primer_apellido" => $request->input("primer_apellido"), "email" => $request->input("email"));  //! C24
         //  echo '<pre>'; print_r($datos); echo '</pre>';
-  
-         //! C25
-        $validator = Validator::make($datos, [ 
-             'primer_nombre'=>'required|string|max:255',
+
+        //! C25
+        $validator = Validator::make($datos, [
+            'primer_nombre' => 'required|string|max:255',
             'primer_apellido' => 'required|string|max:255',
             'email' => 'required|string|email|unique:clientes'
- 
+
         ]);
         //! C25
         if ($validator->fails()) {
-           
+
             $json = array(
-                "detalle"=>"Registro no validos"
+                "detalle" => "Registro no validos"
             );
             return json_encode($json, true);
+        } else {
+            $id_cliente = Hash::make($datos["primer_nombre"] . $datos["primer_apellido"] . $datos["email"]); //! C26
+            $llave_secreta = Hash::make($datos["email"] . $datos["primer_apellido"] . $datos["primer_nombre"], ["rounds" => 12]); //! C26
 
-        }else{
-            $id_cliente = Hash::make($datos["primer_nombre"].$datos["primer_apellido"].$datos["email"]); //! C26
-            $llave_secreta = Hash::make($datos["email"].$datos["primer_apellido"].$datos["primer_nombre"], ["rounds" => 12]); //! C26
-            echo '<pre>'; print_r($id_cliente); echo '</pre>';
-            echo '<pre>'; print_r($llave_secreta); echo '</pre>';
+            $clientes = new Clientes(); //!C27
+
+            $clientes->primer_nombre = $datos["primer_nombre"];
+
+            $clientes->primer_apellido = $datos["primer_apellido"];
+            $clientes->primer_apellido = $datos["primer_apellido"];
+            $clientes->email = $datos["email"];
+            $clientes->id_cliente = $id_cliente;
+            $clientes->llave_secreta = $llave_secreta;
+
+
+
+
+
+            echo '<pre>';
+            print_r($id_cliente);
+            echo '</pre>';
+            echo '<pre>';
+            print_r($clientes->email);
+            echo '</pre>';
+            echo '<pre>';
+            print_r($llave_secreta);
+            echo '</pre>';
         }
- 
-      }
+    }
 }
--- app/Models/Clientes.php
+++ app/Models/Clientes.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class Clientes extends Model
 {
     use HasFactory;
     protected $table = 'clientes';
+
+
+    protected $filliable = [
+        'primer_nombre',
+        'primer_apellido',
+        'email',
+        'id_cliente',
+        'llave_secreta',
+    ]; //! C27
+
+     
+}
